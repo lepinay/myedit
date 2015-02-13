674b951eb855be921189cd9e14925a79b14a7673
Lepinay Laurent <lepinay@gmail.com>
2015-02-13 11:57:16 +0100

    refacto

--- MyEdit/ui.fsx
+++ MyEdit/ui.fsx
@@ -6,7 +6,7 @@
 #r @"packages\Rx-Core.2.2.5\lib\net45\System.Reactive.Core.dll"
 #r @"packages\Rx-Interfaces.2.2.5\lib\net45\System.Reactive.Interfaces.dll"
 #r @"packages\Rx-Linq.2.2.5\lib\net45\System.Reactive.Linq.dll"
-//#r @"packages\FSharpx.TypeProviders.Xaml.1.8.41\lib\40\FSharpx.TypeProviders.Xaml.dll"
+#r @"packages\FSharpx.TypeProviders.Xaml.1.8.41\lib\40\FSharpx.TypeProviders.Xaml.dll"
 
 open System
 open System.Windows          
@@ -15,161 +15,85 @@ open System.Windows.Media
 open System.Windows.Shapes
 open System.Reactive
 open System.Reactive.Linq
-//open FSharpx
+open FSharpx
+open System.Windows.Data
 
-/// This operator is similar to (|>). 
-/// But, it returns argument as a return value.
-/// Then you can chain functions which returns unit.
-let ($) x f = f x ; x
 
-type StackPanel with
-  /// Helper function to compose a GUI
-  member o.add x = o.Children.Add x |> ignore
+let lastChildFill b (dock:DockPanel) = dock.LastChildFill <- b
 
-/// This container is used by some controls to share a variable.
-/// If the value is changed, it fires changed event.
-/// Controls should have this instead of their own internal data
-type SharedValue<'a when 'a : equality>(value:'a) =
-  let mutable _value = value
-  let changed = Event<'a>()
-  member o.Get       = _value
-  member o.Set value =
-    let old = _value 
-    _value <- value
-    if old <> _value then _value |> changed.Trigger
-  member o.Changed = changed.Publish
-type share<'a when 'a : equality> = SharedValue<'a>
+let dock props (content:UIElement list) = 
+    let d = new DockPanel()
+    for p in props do p d
+    for c in content do d.Children.Add(c) |> ignore
+    d
 
-//
-// user control declarations
-//
-/// Volume control , it shows a value and allows you to change it.
-type Volume(title:string, range:int * int, value:share<int>) as this =
-  inherit StackPanel(Orientation=Orientation.Horizontal)
-  do Label(Content=title,Width=50.) |>this.add 
-  let label  = Label(Content=value.Get,Width=50.) $ this.add
-  let slider = Slider(Minimum=float(fst range), Maximum=float(snd range), TickFrequency=2., Width=127.) $ this.add
-  let changedHandler value =
-    label.Content <- string value
-    slider.Value  <- float value
-  do
-    // specifying how to cooperate shared value and slider control
-    slider.ValueChanged.Add(fun arg -> int arg.NewValue |> value.Set)
-    value.Changed.Add changedHandler
+let item name clicks (childrens:MenuItem list) =
+    let i = new Controls.MenuItem(Header=name)
+    for c in childrens do i.Items.Add(c) |> ignore
+    for cl in clicks do
+        i.Click |> Observable.subscribe cl |> ignore
+    i
 
-    changedHandler value.Get // initialization
+let menu items =
+    let m = new Menu()
+    for i in items do m.Items.Add(i) |> ignore
+    DockPanel.SetDock(m,Dock.Top)
+    m
 
-/// Volume control of a color
-type ColorVolume (color:share<Color>) as this =
-  inherit StackPanel(Orientation=Orientation.Vertical)
-  // shared values for controls which represents ARGB of selected color
-  let alpha = SharedValue(int color.Get.A)
-  let red   = SharedValue(int color.Get.R)
-  let green = SharedValue(int color.Get.G)
-  let blue  = SharedValue(int color.Get.B)
-  do
-    // specifying how to calculate dependent shared values
-    let argbChanged = alpha.Changed |> Observable.merge red.Changed |> Observable.merge green.Changed |> Observable.merge blue.Changed
-    argbChanged.Add(fun _ ->
-      color.Set(Color.FromArgb(byte alpha.Get,byte red.Get,byte green.Get,byte blue.Get))
-      )
-    color.Changed.Add(fun color ->
-      alpha.Set (int color.A)
-      red.Set   (int color.R)
-      green.Set (int color.G)
-      blue.Set  (int color.B)
-      )
-    // adding volume controls
-    Volume("Alpha", (0,255), alpha) |> this.add
-    Volume("Red"  , (0,255), red  ) |> this.add
-    Volume("Green", (0,255), green) |> this.add
-    Volume("Blue" , (0,255), blue ) |> this.add
+let grid cols (childrens:UIElement list) =
+    let g = new Grid()
+    for c in cols do g.ColumnDefinitions.Add(new ColumnDefinition(Width=c))
+    for child in childrens do g.Children.Add(child) |> ignore
+    g
 
-[<RequireQualifiedAccess>]
-type MyShapes = Rectangle | Ellipse
-/// Shape container control which reacts when properties of a shape is changed.
-type ShapeContainer(shapes:share<MyShapes>,width:share<int>,height:share<int>,color:share<Color>) as this =
-  inherit Label(Width=250., Height=250.)
-  let mutable shape = Ellipse() :> Shape
-  let setWidth  width  = shape.Width  <- float width
-  let setHeight height = shape.Height <- float height
-  let setColor  color  = shape.Fill   <- SolidColorBrush(color)
-  let initShape () =
-    this.Content <- shape
-    setWidth  width.Get
-    setHeight height.Get
-    setColor  color.Get
-  let setShape du =
-    match du with
-      | MyShapes.Rectangle -> shape <- Rectangle()
-      | MyShapes.Ellipse   -> shape <- Ellipse  ()
-    initShape ()
-  do
-    // specifying cooperations with shared values and the shape
-    width.Changed.Add  setWidth
-    height.Changed.Add setHeight
-    color.Changed.Add  setColor 
-    shapes.Changed.Add setShape
-    // initialization
-    initShape ()
-
-//
-// compose controls
-//
-/// This StackPanel contains every controls in this program
-let stackPanel = StackPanel(Orientation=Orientation.Vertical)
-
-let width = SharedValue(120)
-Volume("Width",(50, 240),width) |> stackPanel.add // add a volume to the StackPanel
-
-let height = SharedValue(80)
-Volume("Height",(50, 200),height) |> stackPanel.add // add a volume to the StackPanel
-
-let color = SharedValue(Colors.Blue)
-ColorVolume(color) |> stackPanel.add // add volumes to the StackPanel
-
-let shapes = SharedValue(MyShapes.Ellipse)
-let ellipseButton   = Button(Content="Ellipse")   $ stackPanel.add
-let rectangleButton = Button(Content="Rectangle") $ stackPanel.add
-ellipseButton.Click.Add(  fun _ -> shapes.Set MyShapes.Ellipse)   // add event handler to fire dependency calculation
-rectangleButton.Click.Add(fun _ -> shapes.Set MyShapes.Rectangle)
-
-// This is a shape control shown in the bottom of this program's window
-ShapeContainer(shapes,width,height,color) |> stackPanel.add
-
-// Make a window and show it
-let dock = new DockPanel()
-let menu = new Menu()
-let file = new MenuItem(Header="_File")
-let openFolder = new MenuItem(Header="Open folder")
-openFolder.Click |> Observable.subscribe (fun e -> printfn "click")
-
-file.Items.Add(openFolder)
-file.Items.Add(new MenuItem(Header="Open folder"))
-file.Items.Add(new MenuItem(Header="save"))
-menu.Items.Add(file)
-dock.Children.Add(menu)
-
-let grid = new Grid()
-grid.ColumnDefinitions.Add(new ColumnDefinition(Width=new GridLength(1.,GridUnitType.Star)))
-grid.ColumnDefinitions.Add(new ColumnDefinition(Width=new GridLength(5.)))
-grid.ColumnDefinitions.Add(new ColumnDefinition(Width=new GridLength(2.,GridUnitType.Star)))
-dock.Children.Add(grid)
 let tree = new TreeView()
-type MenuItem = 
-    {
-        title:String
-    }
+type MenuItem() =
+    member val Title = "" with get, set
+    member val Items:MenuItem list = [] with get, set 
+
+let verticalAlignment a (gs:GridSplitter) = gs.VerticalAlignment <- a
+let horizontalAlignment a (gs:GridSplitter) = gs.HorizontalAlignment <- a
+let resizeDirection a (gs:GridSplitter) = gs.ResizeDirection <- a
+let showsPreview a (gs:GridSplitter) = gs.ShowsPreview <- a
+let width a (gs:GridSplitter) = gs.Width <- a
+let column a (gs:GridSplitter) = Grid.SetColumn(gs,a)
+
+//https://social.msdn.microsoft.com/Forums/vstudio/en-US/acc87765-618e-4afd-b695-df5144d904ac/hierarchicaldatatemplate-for-treeview-programmatically-c?forum=wpf    
 let template = new HierarchicalDataTemplate(typeof<MenuItem>)
 let labelFactory = new FrameworkElementFactory(typeof<TextBlock>);
-template.ItemsSource = "Items"
-//https://social.msdn.microsoft.com/Forums/vstudio/en-US/acc87765-618e-4afd-b695-df5144d904ac/hierarchicaldatatemplate-for-treeview-programmatically-c?forum=wpf
+labelFactory.SetBinding(TextBlock.TextProperty, new Binding("Title"));
+template.ItemsSource <- new Binding("Items")
+template.VisualTree <- labelFactory
+tree.ItemTemplate <- template
+tree.ItemsSource <- [MenuItem(Title="Hello :)", Items=[MenuItem(Title="World")])]
 
-
-//template.DataType = "MenuItem"
-tree.ItemTemplate = template
 Grid.SetColumn(tree,0)
-grid.Children.Add(tree)
 
-let window = Window(Title="F# is fun!",Width=260., Height=420., Content = dock, Topmost = true)
+
+
+let splitter props = 
+    let gs = new GridSplitter(VerticalAlignment=VerticalAlignment.Stretch,HorizontalAlignment=HorizontalAlignment.Center,ResizeDirection=GridResizeDirection.Columns,ShowsPreview=true,Width=5.)
+    for p in props do p gs
+    gs
+
+
+
+
+let window = Window(Title="F# is fun!",Width=260., Height=420., Topmost = true)
+window.Content <- 
+    dock  [lastChildFill true]
+            [menu 
+                [item "_File" []
+                    [item "Open folder" [(fun e -> printfn "click")] [];
+                        item "Save" [] []]];
+            grid [GridLength(1.,GridUnitType.Star);
+                  GridLength(5.);
+                  GridLength(2.,GridUnitType.Star)]
+                  [tree;splitter [column 1; verticalAlignment VerticalAlignment.Stretch;horizontalAlignment HorizontalAlignment.Center;resizeDirection GridResizeDirection.Columns; showsPreview true;width 5.] ]]
 window.Show()
+
+
+
+
+
+
